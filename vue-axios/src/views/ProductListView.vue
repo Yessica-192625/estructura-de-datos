c9077c4d0dcd072223c6fafa8dfecef1debe94f5
Yessica Bayona <ypbayonam@ufpso.edu.co>
2026-05-27 19:32:15 -0500

    edit

--- vue-axios/src/views/ProductListView.vue
+++ vue-axios/src/views/ProductListView.vue
@@ -13,9 +13,7 @@
       @delete="handleDelete"
     />
 
-    <div class="mt-3">
-      <button class="btn btn-success" @click="goCreate">Agregar producto</button>
-    </div>
+    
   </div>
 </template>
 
